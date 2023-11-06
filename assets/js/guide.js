7e279c59d8cc379da529a5bbc9f819359f014746
eunha <eunha0kim@gmail.com>
2023-11-06 23:32:38 +0900

    tabs

--- assets/js/guide.js
+++ assets/js/guide.js
@@ -1,44 +1,41 @@
 var UI_GUIDE = UI_GUIDE || {}; 
 
-var gnbDom = document.querySelector('.guide-gnb');
-
 UI_GUIDE.GNB = {
     init : function(){
+        this.reset();
         this.setButton();
     },
     reset : function(){
-        console.log('reset:')
+        var _ulDom = document.querySelector('.guide-gnb')
+        var _liObj = _ulDom.children;
+        console.log(_ulDom)
+        console.log(_liObj)
+
+        for (var i=0; i<_liObj.length; i++) {
+            console.log(_liObj[i])
+            _liObj[i].querySelector('button').classList.remove('on')
+        }
     },
     setButton: function(){
         var _self = this;
-        console.log('setButton:')
         var gnbDom = document.getElementsByClassName('guide-gnb')[0];
-        var gnbLi = gnbDom.children;
-        var gnbBtn = gnbLi.children;
-
-        console.log('gnbLi:');
-        console.log(gnbLi);
-        console.log(gnbLi.length);
-        console.log(gnbLi[0]);
-        console.log(typeof gnbLi);
-
-
-        for (var i=0; i < gnbLi.length; i++) {
-            if ( gnbLi[i].getAttribute('id') === 'component' ){
-                console.log('component');
-            } else if ( gnbLi[i].getAttribute('id') === 'template' ){
-                console.log('component');
-            } else if ( gnbLi[i].getAttribute('id') === 'template' ){
-                console.log('component');
-            }
-        }
-        
+        var iframeDom = document.getElementById('router');
 
         gnbDom.addEventListener('click',function(e){
-            console.log('e:')
-            console.log(e)
-        })
+            var _id = e.target.closest('li').getAttribute('id');
+            _self.reset();
 
+            if ( _id === 'component' ){
+                console.log(e.target)
+                e.target.classList.add('on')
+                iframeDom.setAttribute('src','./component/component.html')
+            } else if ( _id === 'template' ){
+                e.target.classList.add('on')
+                iframeDom.setAttribute('src','./template/template.html')
+            } else if ( _id === 'page' ){
+                e.target.classList.add('on')
+                iframeDom.setAttribute('src','./pages/pageList.html')
+            }
+        })
     }
-    
 }
